6b1979277c93159713189e96d032e2501d91622c
shikharsaxena26 <shikharsaxena26@yahoo.in>
2015-05-24 00:04:48 +0530

    Programming assignment 2
    Committed the file containing the required changes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix.R contains two functions makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## The makeCacheMatrix takes a square matrix as an argument. It stores the matrix and its 
+## inverse for future purpose. Both matrix and its inverse can be set and retrieved using the
+## respective setter and getter functions. 
 
 makeCacheMatrix <- function(x = matrix()) {
-print(x)
+  inverse <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function() x
+  setInverse <- function(matrix_inverse) inverse <<- matrix_inverse
+  getInverse <- function() inverse
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. If inverse is already stored, returns the 
+## cached inverse. Else, computes & retrieves the inverse, and stores it for future reference.
+## If the matrix is non-invertible, it returns an appropriate message.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheFunctionsList, ...) {
+  inverse <- cacheFunctionsList$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- cacheFunctionsList$getMatrix()
+  num_rows<-nrow(data)
+  num_col<-ncol(data)
+  if(num_rows!=num_col){
+    message("The matrix is not a square matrix. Hence non-invertible!!")
+    return()
+  }
+  if(det(data)==0){
+    message("The determinant of the matrix is zero. Hence non-invertible!!")
+    return()
+  }
+  inverse <- solve(data)
+  cacheFunctionsList$setInverse(inverse)
+  inverse 
 }
